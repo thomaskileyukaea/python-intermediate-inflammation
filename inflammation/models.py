741d6230b5961d3d05c4ff6c9e2c61af79f13af7
Thomas Kiley <thomas.kiley@ukaea.uk>
2023-07-27 16:26:48 +0100

    Better name for the mean

--- inflammation/models.py
+++ inflammation/models.py
@@ -35,10 +35,10 @@ def daily_min(data):
 
 def s_dev(data):
     """Computes and returns standard deviation for data."""
-    mmm = np.mean(data, axis=0)
+    number = np.mean(data, axis=0)
     devs = []
     for entry in data:
-        devs.append((entry - mmm) * (entry - mmm))
+        devs.append((entry - number) * (entry - number))
 
     s_dev2 = sum(devs) / len(data)
     return {'standard deviation': s_dev2}
